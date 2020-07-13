6862f68d422937f5bfca7da834cc7f4c934fa501
Ismael <isouest@hotmail.com>
2020-07-13 11:38:58 +0200

    [SHOW-71-72] feat: Crear componente tarjeta mini y sliders index

--- frontend/src/main/resources/webapp/src/app/concert.ts
+++ frontend/src/main/resources/webapp/src/app/concert.ts
@@ -2,9 +2,54 @@ export interface Concert {
     name: String;
     description: String;
     imgPath: String;
+    date: Date;
 }
 
-export const CONCERTS=[{name: 'Riverland 2020', description: 'El festival Riverland es un festival de música independiente celebrado en la localidad de Arriondas (provincia de Asturias, España) e inaugurado la primera semana de agosto del 2019. ', imgPath: 'https://www.destinosella.com/images/riverland-arriondas.jpg'},
-{name: 'Riverland 2020', description: 'El festival Riverland es un festival de música independiente celebrado en la localidad de Arriondas (provincia de Asturias, España) e inaugurado la primera semana de agosto del 2019. ', imgPath: 'https://www.destinosella.com/images/riverland-arriondas.jpg'},
-{name: 'Riverland 2020', description: 'El festival Riverland es un festival de música independiente celebrado en la localidad de Arriondas (provincia de Asturias, España) e inaugurado la primera semana de agosto del 2019. ', imgPath: 'https://www.destinosella.com/images/riverland-arriondas.jpg'},
-{name: 'Riverland 2020', description: 'El festival Riverland es un festival de música independiente celebrado en la localidad de Arriondas (provincia de Asturias, España) e inaugurado la primera semana de agosto del 2019. ', imgPath: 'https://www.destinosella.com/images/riverland-arriondas.jpg'}]
+
+export const CONCERTS = [
+    {
+        name: 'Riverland 2020',
+        description: 'El festival Riverland es un festival de música independiente celebrado en la localidad de Arriondas (provincia de Asturias, España) e inaugurado la primera semana de agosto del 2019. ',
+        imgPath: 'https://www.destinosella.com/images/riverland-arriondas.jpg',
+        date: '19/08/2020-22/08/2020',
+        streamingPath: ''
+    },
+
+    {
+        name: 'O Son do Camiño',
+        description: 'El Festival O Son do Camiño se aplaza a 2021: será los días 17, 18 y 19 de junio en O Monte do Gozo, Santiago de Compostela',
+        imgPath: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F101055718%2F291672116228%2F1%2Foriginal.20200515-100816?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=145%2C0%2C832%2C416&s=1f1417ccb53637e98bdeb667574ce9a9',
+        date: '27/08/2020-30/08/2020'
+    },
+
+    {
+        name: 'Estopa',
+        description: 'Estopa es un dúo español de música pop formado por los hermanos David y José Manuel Muñoz. Creado el 18 de octubre de 1999, el grupo es natural de la ciudad de Cornellá de Llobregat, comarca del Bajo Llobregat.​',
+        imgPath: 'https://s1.eestatic.com/2019/08/26/cultura/musica/Censura-Flamenco-Cataluna-Drogas-Policia_Nacional-Musica_424469138_133101664_1706x1280.jpg',
+        date: '11/09/2020',
+        streamingPath: 'www.youtube.com'
+    },
+
+    {
+        name: 'David Bisbal',
+        description: 'David Bisbal Ferre es un cantante español, que alcanzó la popularidad en el año 2001 tras su participación en la primera edición de Operación Triunfo, donde quedó en segundo lugar. Sus ventas mundiales se estiman en más de 6 millones de copias.',
+        imgPath: 'https://as.com/tikitakas/imagenes/2019/03/15/portada/1552634939_107631_1552649802_noticia_normal.jpg',
+        date: '08/10/2020',
+        streamingPath: ''
+    },
+
+    {
+        name: 'Estopa',
+        description: 'Estopa es un dúo español de música pop formado por los hermanos David y José Manuel Muñoz. Creado el 18 de octubre de 1999, el grupo es natural de la ciudad de Cornellá de Llobregat, comarca del Bajo Llobregat.​',
+        imgPath: 'https://s1.eestatic.com/2019/08/26/cultura/musica/Censura-Flamenco-Cataluna-Drogas-Policia_Nacional-Musica_424469138_133101664_1706x1280.jpg',
+        date: '11/09/2020',
+        streamingPath: 'www.youtube.com'
+    },
+
+    {
+        name: 'David Bisbal',
+        description: 'David Bisbal Ferre es un cantante español, que alcanzó la popularidad en el año 2001 tras su participación en la primera edición de Operación Triunfo, donde quedó en segundo lugar. Sus ventas mundiales se estiman en más de 6 millones de copias.',
+        imgPath: 'https://as.com/tikitakas/imagenes/2019/03/15/portada/1552634939_107631_1552649802_noticia_normal.jpg',
+        date: '08/10/2020',
+        streamingPath: ''
+    }] 
